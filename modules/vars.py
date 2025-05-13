23134b121bc5d3c64c10689b9f6571fc25a4bc4b
Error1234543 <ptaviyad21@gmail.com>
2025-05-13 09:38:01 +0000

    sonic

--- modules/vars.py
+++ modules/vars.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "20619533")
+API_HASH  = os.environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8163771227:AAFCYqxXM9BNjiqMMS0S6tRLOK1A_JQugNc") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
