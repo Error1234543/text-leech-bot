4aa22fc8601abb60bbe8cdcb563f8f75f65f5a8e
Error1234543 <ptaviyad21@gmail.com>
2025-10-26 00:11:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 API_ID    = os.environ.get("API_ID", "20619533")
 API_HASH  = os.environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8176277339:AAFwc7xY8cTWyUNVm3Q3_HkyJXTWRyH_kkM") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8483889672:AAHF218gaOzYjnoarBrJhNylBmw2C2H65CE") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
